a2c73e471f47290e22cb5f2ef1d551a6206c54af
mhasaero <eroouji7314@gmail.com>
2024-09-07 21:56:18 +0700

    feat: add react router dom and tanstack

---
+++ src/App.router.tsx
@@ -0,0 +1,25 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import { RouteObject } from "react-router-dom";
+import AppLayout from "./pages/Layout/AppLayout";
+import Home from "./pages/Home";
+
+const RouterBuilder = () => {
+  //Only for client to see
+  const generalRoutes: RouteObject[] = [
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ];
+
+  const routes = createBrowserRouter([
+    {
+      element: <AppLayout />,
+      children: generalRoutes,
+    },
+  ]);
+
+  return routes;
+};
+
+export default RouterBuilder;
