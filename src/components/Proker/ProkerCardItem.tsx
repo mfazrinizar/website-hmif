7efde1a8967919b25574de454281c2e7ad2ef155
RA. Nur Afifah Widyadhana <widyadhana92@gmail.com>
2024-10-03 22:41:52 +0700

    fix(proker): fixing card responsiveness

--- src/components/Proker/ProkerCardItem.tsx
+++ src/components/Proker/ProkerCardItem.tsx
@@ -11,34 +11,38 @@ type Props = {
 
 export default function ProkerCardItem({eventFormat, name, date, description, dinas} : Props) {
   return (
-    <div className="mb-7 flex w-full h-fit rounded-2xl border border-black p-4 md:p-7 lg:w-11/12">
-      <div className="flex flex-col md:flex-row gap-6 md:gap-0">
+    <div className="mb-7 flex w-full h-fit md:h-auto rounded-2xl border border-black p-4 md:p-7 lg:w-11/12">
+      <div className="flex flex-col w-full h-fit md:h-auto md:flex-row gap-6 md:gap-0">
       <img
-      src={`img/proker/${dinas}/${name}.png`}
-      alt={`${name}`}
-      className="mb-4 w-full h-1/2 md:w-1/2 md:mb-0 lg:w-1/4 md:h-full"
+        src={`img/proker/${dinas}/${name}.png`}
+        alt={`${name}`}
+        className="mb-4 w-full h-1/2 md:w-1/3 md:mb-0 lg:w-1/4 md:h-full object-cover"
       />
-      <div className="flex flex-col gap-2 justify-between items-start md:pl-14">
+      <div className="flex flex-col gap-2 justify-between items-start md:pl-8 lg:pl-14">
       <p className="inline-block rounded bg-primary px-2 py-1 text-white">
         {eventFormat}
       </p>
       <div className="flex flex-col gap-3 items-start">
-      <h1 className="text-xl md:text-3xl font-medium text-primary">
-        {name}
-      </h1>
-      <div className="flex gap-2 items-center justify-center text-primary text-sm">
-        <Clock/>
-        <p>{date}</p>
-        <p>| {dinas}</p>
-      </div>
-      <p className="text-left pb-5 pt-2">
-        {description}
-      </p>
-      </div>
-      <Button className="border-2 border-primary bg-[#F5F5F5] text-primary">
-        Learn more
-      </Button>
-    </div>
+        <div className="overflow-hidden text-ellipsis line-clamp-1">
+          <h1 className="text-start text-xl md:text-3xl font-medium text-primary">
+            {name}
+          </h1>
+        </div>
+        <div className="flex gap-2 items-center justify-center text-primary text-sm">
+          <Clock/>
+          <p>{date}</p>
+          <p>| {dinas}</p>
+        </div>
+        <div className="pb-5 pt-2 w-fit max-h-32 overflow-hidden text-ellipsis line-clamp-5">
+          <p className="text-left">
+            {description}
+          </p>
+        </div>
+        </div>
+          <Button className="border-2 border-primary bg-[#F5F5F5] text-primary">
+            Learn more
+          </Button>
+        </div>
       </div>
   </div>
   )
--- src/components/Proker/ProkerCards.tsx
+++ src/components/Proker/ProkerCards.tsx
@@ -17,18 +17,24 @@ export default function ProkerCards({ dinas } : Props) {
       }, []);
 
   return (
-    <div className="flex justify-center items-center flex-col px-2 md:w-full md:px-8">
+    <div className="flex justify-center items-center flex-col w-full">
         {
             proker.map((proker, key) => (
                 <ProkerCardItem key={key} name={proker["name"]} eventFormat={proker["eventFormat"]} date={proker["date"]} description={proker["description"]} dinas={dinas}/>
             ))
         }
+
     {/* See more Button */}
     {
-        proker.length >= 3 ? (
-        <Button className="w-full rounded-2xl border-2 border-primary bg-[#F5F5F5] py-6 text-lg text-primary md:w-11/12">
+        proker.length >= 3 ? ( 
+        <div className="w-full bg-slate-200">
+        <Button className="w-full rounded-2xl border-2 border-primary bg-[#F5F5F5] py-6 text-lg text-primary lg:w-11/12">
             See more
-        </Button>) : ""
+        </Button>
+        </div>
+
+         ) 
+        : ""
     }
     
     </div>
