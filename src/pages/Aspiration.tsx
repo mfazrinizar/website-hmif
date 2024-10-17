5114a378eb91373f7b2489be9fb6f10fd85070fd
mhasaero <eroouji7314@gmail.com>
2024-10-17 22:36:48 +0700

    feat(aspiration): add accordion aspiration

--- src/pages/Aspiration.tsx
+++ src/pages/Aspiration.tsx
@@ -1,4 +1,10 @@
 import { FormAspiration } from "@/components/Aspiration/FormAspiration";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Aspiration() {
   return (
@@ -15,6 +21,57 @@ export default function Aspiration() {
       <div className="rounded-xl border-2 border-muted">
         <FormAspiration />
       </div>
+      <Accordion type="single" collapsible className="space-y-8 md:w-1/2">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Apa tujuan aspirasi ini ?</AccordionTrigger>
+          <AccordionContent>
+            Aspirasi ini dibuat untuk mengumpulkan umpan balik, saran, dan ide
+            dari mahasiswa agar dapat membantu meningkatkan kegiatan, program,
+            dan organisasi kami secara keseluruhan.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>
+            Bisakah saya mengirimkan aspirasi secara anonim ?
+          </AccordionTrigger>
+          <AccordionContent>
+            Ya, Anda dapat memilih untuk tidak mengisi kolom nama jika Anda
+            lebih suka tetap anonim. Namun, memberikan informasi kontak Anda
+            dapat membantu kami untuk menindaklanjuti aspirasi Anda jika
+            diperlukan.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>
+            Bagaimana cara mengirimkan aspirasi saya ?
+          </AccordionTrigger>
+          <AccordionContent>
+            Anda dapat mengirimkan aspirasi melalui formulir yang disediakan di
+            halaman ini. Pastikan untuk menyertakan semua detail yang relevan
+            agar kami dapat memahami aspirasi Anda dengan lebih baik.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-4">
+          <AccordionTrigger>
+            Apa yang terjadi setelah mengirimkan aspirasi saya ?
+          </AccordionTrigger>
+          <AccordionContent>
+            Setelah Anda mengirimkan aspirasi Anda, tim kami akan meninjau umpan
+            balik tersebut. Kami akan mempertimbangkan masukan Anda dalam
+            perencanaan dan pengambilan keputusan. Jika diperlukan, kami mungkin
+            akan menghubungi Anda untuk klarifikasi lebih lanjut.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-5">
+          <AccordionTrigger>
+            Apa aspirasi saya akan dijaga kerahasiaannya ?
+          </AccordionTrigger>
+          <AccordionContent>
+            Ya, semua pengiriman akan diperlakukan dengan kerahasiaan. Identitas
+            dan informasi pribadi Anda tidak akan dibagikan tanpa izin Anda.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </section>
   );
 }
