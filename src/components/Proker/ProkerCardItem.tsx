c97b529580d28ffdf80e3bd70a0b0aa0e4932f22
RA. Nur Afifah Widyadhana <widyadhana92@gmail.com>
2024-10-22 14:34:28 +0700

    fix(proker): fix card image

--- src/components/Proker/ProkerCardItem.tsx
+++ src/components/Proker/ProkerCardItem.tsx
@@ -26,15 +26,24 @@ export default function ProkerCardItem({
     navigate(`/proker/${nav.toLowerCase()}/${formattedTitle}`); // Gunakan title sebagai bagian dari URL
   };
 
+  const formattedImg = name.split(" ").join("%20");
+  console.log(dinas, "/", formattedImg);
+
   return (
     <div className="mb-7 flex h-fit w-full rounded-2xl border border-black p-4 md:h-auto md:p-7 lg:w-11/12">
       <div className="flex h-fit w-full flex-col gap-6 md:h-auto md:flex-row md:gap-0">
-        <img
+        {/* <img
           src={`img/proker/${dinas}/${name}.png`}
           alt={`${name}`}
           className="mb-4 w-full rounded-lg object-cover md:mb-0 md:w-1/3 lg:w-1/4"
-        />
-        <div className="flex flex-col items-start justify-between gap-2 md:pl-8 lg:pl-14">
+        /> */}
+        <div
+          className={`mb-4 h-52 w-full rounded-lg bg-cover bg-center md:mb-0 md:w-1/3 lg:h-80 lg:w-1/4`}
+          style={{
+            backgroundImage: `url('/img/proker/${dinas}/${name}.png')`,
+          }}
+        ></div>
+        <div className="h- flex w-full flex-col items-start justify-between gap-2 md:w-2/3 md:pl-8 lg:h-80 lg:w-3/4 lg:pl-14">
           <p className="inline-block rounded bg-primary px-2 py-1 text-white">
             {eventFormat}
           </p>
@@ -64,5 +73,42 @@ export default function ProkerCardItem({
         </div>
       </div>
     </div>
+    // <div className="lg:w- mb-7 h-80 w-full rounded-2xl border border-black bg-slate-300 px-5">
+    //   <div className="item flex h-full w-full items-center gap-5">
+    //     <img
+    //       src={`img/proker/${dinas}/${name}.png`}
+    //       alt={name}
+    //       className="h-80 w-96 object-cover"
+    //     />
+    //     <div className="flex flex-col items-start justify-between gap-2 md:pl-8 lg:pl-14">
+    //       <p className="inline-block rounded bg-primary px-2 py-1 text-white">
+    //         {eventFormat}
+    //       </p>
+    //       <div className="flex flex-col items-start gap-3">
+    //         <div className="line-clamp-1 overflow-hidden text-ellipsis">
+    //           <h1 className="text-start text-xl font-medium text-primary md:text-3xl">
+    //             {name}
+    //           </h1>
+    //         </div>
+    //         <div className="flex items-center justify-center gap-2 text-sm text-primary">
+    //           <Clock />
+    //           <div className="flex flex-row flex-wrap gap-1 text-start">
+    //             <p>{date}</p>
+    //             <p>| {dinas}</p>
+    //           </div>
+    //         </div>
+    //         <div className="line-clamp-5 max-h-32 min-h-32 w-fit overflow-hidden text-ellipsis pb-5 pt-2">
+    //           <p className="text-left">{description}</p>
+    //         </div>
+    //       </div>
+    //       <Button
+    //         onClick={handleLearnMore}
+    //         className="border-2 border-primary bg-[#F5F5F5] text-primary"
+    //       >
+    //         Learn more
+    //       </Button>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
